7cf6f20c10067b0af079b19380d0667b0f689c8c
enarss <enarss@outlook.com>
2025-01-29 12:26:42 +0330

    search task

--- JS/script.js
+++ JS/script.js
@@ -4,12 +4,15 @@ const taskInput = document.querySelector(".task__input");
 const taskList = document.querySelector(".task__list");
 const taskAlert = document.querySelector(".task__alert");
 const continer = document.querySelector(".task");
+const searchBox = document.querySelector(".search__input");
 let taskArr = [];
+let listArr = [];
 
-function CreatTask(title, check, date) {
+function CreatTask(title, check, date, editButtonId) {
     this.title = title;
     this.check = check;
     this.date = date;
+    this.editButtonId = editButtonId;
 }
 
 function addTask() {
@@ -32,6 +35,7 @@ function addTask() {
         if (complateTask.checked) complated.play();
     });
 
+
     const title = document.createElement("p");
     title.innerText = taskInput.value;
     titleDiv.appendChild(title);
@@ -46,19 +50,20 @@ function addTask() {
 
     const editButton = document.createElement("button");
     editButton.classList = "btn edt__btn";
+    const editButtonId = Math.random();
+    editButton.id = editButtonId;
     editButton.innerText = "ویرایش";
     buttonDiv.appendChild(editButton);
     editButton.addEventListener("click", () => {
-        taskInput.value = title;
+        taskInput.value = title.value;
     });
-    
-    let newTask = new CreatTask(taskInput.value, false, 0);
-    taskArr.push(newTask);
 
-    const newList = document.createElement("h2");
-    newList.innerText = "انجام شده";
-    continer.appendChild(newList);
+    let newTask = new CreatTask(taskInput.value, false, 0, editButtonId);
+    taskArr.push(newTask);
+    listArr.push({ element: task });
 }
+
+//input
 
 addButton.addEventListener("click", () => {
     if (taskInput.value.length < 3)
@@ -78,7 +83,20 @@ taskInput.addEventListener("keypress", (e) => {
             taskAlert.classList = "task__alert";
             addTask();
             taskInput.value = "";
-            console.log(taskArr);
+            //console.log(listArr[0].element);
         }
     }
+});
+
+// search
+
+searchBox.addEventListener("input", (e) => {
+    const searchText = e.target.value;
+    console.log(searchBox);
+    listArr.forEach((item) => {
+        const itemText = item.element.innerText;
+        console.log(itemText);
+        if (itemText.includes(searchText)) item.element.classList.remove("hidden");
+        else item.element.classList.add("hidden");
+    });
 });
